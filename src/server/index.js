5addd36910b61da62d042098da293d8d7ad87548
RashmitShrestha <rashmit_stha@gmail.com>
2020-04-19 15:59:53 -0700

    Updated

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config();
+//Code Academy in the Back - End section helped tons after looking at other people's code to get an idea
 //Some help gotten from Eyong Kevin
+//place these dependencies from npm
 var path = require('path');
 const express = require('express');
 const app = express();
@@ -9,13 +11,9 @@ const bodyParser = require('body-parser');
 
 var aylien = require("aylien_textapi");
 // set aylien API credentials
-// NOTICE that textapi is the name I used, but it is arbitrary.
-// You could call it aylienapi, nlp, or anything else, 
-//   just make sure to make that change universally!
 var textapi = new aylien({
     application_id: process.env.API_ID,
-    application_key: process.env.API_KEY
-});
+    application_key: process.env.API_KEY});
 
 //some middleware
 app.use(cors());
@@ -25,34 +23,24 @@ app.use(express.static('dist'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-
 app.get('/', function(req, res) {
     res.sendFile('dist/index.html');
 })
 
-// designates what port the app will listen to for incoming requests
-app.listen(8081, function() {
-    console.log('Example app listening on port 8081!')
+// Listen for a port
+app.listen(3000, function() {
+    console.log('Example app listening on port 3000!')
 })
-
 
 app.post('/end', function (req, res) {
     const APIurl = req.body.text;
-
-    //help from Aylien Documentation
+    //help from Aylien Documentation https://docs.aylien.com/textapi/sdks/#node-js-sdk
     textapi.sentiment({
       url: APIurl,
       mode: 'document'
     }, function (error, response) {
       res.send(response)
-      if (error === null) {
-        console.log('error');
-      }
     })
   });
-  
-
-
-
   
   module.exports = app;
